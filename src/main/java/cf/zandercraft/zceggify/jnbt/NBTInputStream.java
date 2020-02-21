5b598643dfa61cee1ddb57ccc6b78e372fcf47aa
Zander <28664144+ZandercraftGames@users.noreply.github.com>
2020-02-21 00:15:05 -0500

    2nd fix

--- src/main/java/cf/zandercraft/zceggify/jnbt/NBTInputStream.java
+++ src/main/java/cf/zandercraft/zceggify/jnbt/NBTInputStream.java
@@ -17,7 +17,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.sk89q.jnbt;
+package cf.zandercraft.zceggify.jnbt;
 
 import java.io.Closeable;
 import java.io.DataInputStream;
